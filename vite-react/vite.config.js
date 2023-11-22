892bdcbe255149686fb5ce6308f33185393c71f4
myu <myu_uehara@mail.toyota.co.jp>
2023-11-22 11:53:08 +0900

    1153

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES // この行を追加
+    ? 'solo-front' // この行を追加
+    : './', // この行を追加
   plugins: [react()],
-})
+});
