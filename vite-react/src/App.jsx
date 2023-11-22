468b0f12adee3f5a7071f962be8050411986f488
myu <myu_uehara@mail.toyota.co.jp>
2023-11-22 11:29:49 +0900

    1129

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,127 @@
+import { useEffect, useState } from 'react';
+
+function App({ getAPI, postAPI, deleteAPI }) {
+  const [lists, setLists] = useState();
+  const [tasks, setTasks] = useState();
+  const [discs, setDiscs] = useState();
+  const [newT, setNewT] = useState('');
+  const [newD, setNewD] = useState('');
+
+  useEffect(() => {
+    getAPI(1).then(setLists);
+    getAPI(2).then(setTasks);
+  }, []);
+  // console.log('tasks : ', tasks);
+
+  async function addTask(e) {
+    e.preventDefault();
+    try {
+      const obj = { title: newT, description: newD };
+      const back = await postAPI(2, obj);
+      console.log('back to App: ', back);
+      const T = back[0][title];
+      const D = back[0][description];
+      console.log('T & D: ', T, D);
+      setTasks({ ...tasks, T });
+      setTasks({ ...discs, D });
+      setNewT('');
+      setNewD('');
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+  return (
+    <div>
+      <h1>メモ帳</h1>
+      <main>
+        <form onSubmit={(e) => addTask(e)}>
+          <input
+            value={newT}
+            placeholder="タイトル :"
+            onChange={(e) => setNewT(e.target.value)}
+          />
+          <input
+            value={newD}
+            placeholder="内容 :"
+            onChange={(e) => setNewD(e.target.value)}
+          />
+          <button>追加</button>
+        </form>
+        {tasks &&
+          tasks.map((task) => {
+            async function deleteTask() {
+              try {
+                deleteAPI(2, task.id);
+                setTasks((tasks) => tasks.filter((e) => e !== task));
+              } catch (error) {
+                alert(error.message);
+              }
+            }
+            function setTask(value) {
+              setTasks((tasks) => tasks.map((e) => (e === task ? value : e)));
+            }
+            function setD(value) {
+              setDiscs((discs) => discs.map((e) => (e === task ? value : e)));
+            }
+            // function setCompleted(completed) {
+            //   setTask({ ...task, completed });
+            // }
+            // async function setCompleted(completed) {
+            //   setTask(await taskRepo.save{ ...task, completed });
+            // } // >>> PUT
+            function setTitle(title) {
+              setTask({ ...task, title });
+            }
+            function setDis(dis) {
+              setD({ ...task, dis });
+            }
+            // async function doSaveTask(){
+            //   try {
+            //     setTask(await taskRepo.save(task)); //PUT
+            //   } catch (error) {
+            //     alert(error.message);
+            //   }
+            // }
+            return (
+              <div key={task.id}>
+                {/* <input
+                  type="checkbox"
+                  checked={task.completed}
+                  onChange={(e) => setCompleted(e.target.checked)}
+                /> */}
+                <p>No.{task.id}</p>
+                <input
+                  value={task.title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <input
+                  value={task.description}
+                  onChange={(e) => setDis(e.target.value)}
+                />
+                {/* <button onClick={() => doSaveTask()}>保存</button> */}
+                <button onClick={() => deleteTask()}>削除</button>
+              </div>
+            );
+          })}
+      </main>
+      <h1>カリキュラム</h1>
+      <div>
+        {lists &&
+          lists.map((list) => {
+            return (
+              <div>
+                week : {list.week}{' '}
+                <a id={list.type} href={list.url} target="_blank">
+                  <strong>{list.title}</strong>
+                </a>
+                {list.goal && <p> {list.goal}</p>}
+                {list.memo && <p> {list.memo}</p>}
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-react/src/main.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+
+console.log('import.meta.env : ', import.meta.env);
+// let res = await fetch('http://localhost:8000/hi', {
+// let url = process.env.SERVER_URL || 'http://localhost:8000';
+// let url = 'http://localhost:8000';
+const url = 'http://localhost:8000';
+
+const getAPI = async (table, req) => {
+  console.log('req front-GET: ', req);
+  let res = await fetch(`${url}/api/${table}`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    // body: JSON.stringify(req),
+  });
+  console.log('res : ', res);
+  let text = await res.text();
+  try {
+    console.log('GET return : ', JSON.parse(text));
+    return JSON.parse(text);
+  } catch (error) {
+    console.error('Received this from server:', text);
+    throw new Error('Server is down');
+  }
+};
+
+const postAPI = async (table, req) => {
+  console.log('req front-POST: ', req);
+  let res = await fetch(`${url}/api/${table}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(req),
+  });
+  console.log('res : ', res);
+  let text = await res.text();
+  try {
+    console.log('POST return : ', text);
+  } catch (error) {
+    console.error('Received this from server:', text);
+    throw new Error('Server is down');
+  }
+};
+
+const deleteAPI = async (table, id, req) => {
+  console.log('req front-POST: ', req);
+  let res = await fetch(`${url}/api/${table}/${id}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    // body: JSON.stringify(req),
+  });
+  console.log('res : ', res);
+  let text = await res.text();
+  try {
+    console.log('DELETE return : ', text);
+  } catch (error) {
+    console.error('Received this from server:', text);
+    throw new Error('Server is down');
+  }
+};
+
+// const putOBJ = {
+//   title: 'next todo:環境変数',
+//   description: 'Viteの環境変数がわけわからん...',
+//   memo: 'いったん仕切り直し！',
+// };
+// postAPI(2, putOBJ);
+// deleteAPI(2, 15);
+getAPI(1);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App getAPI={getAPI} postAPI={postAPI} deleteAPI={deleteAPI} />
+  </React.StrictMode>
+);
